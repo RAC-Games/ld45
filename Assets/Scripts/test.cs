fb238208a5cc9564802a2c32ec2c6e058eac0b92
Chris <c.rausch020@gmail.com>
2019-10-04 19:59:14 +0200

    curve inspector added

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [Curve(0, 0, 3f, 5f, true)]
+    public AnimationCurve curveX;
+
+    [Curve(0, 0f, 3f, 5f, true)]
+    public AnimationCurve curveY;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Time.time);
+        transform.position = new Vector3(curveX.Evaluate(Time.time), curveY.Evaluate(Time.time), 0);
+    }
+}
