406d46b2920eba3f2b4f533f16995f587fa68e04
Luisa Kalkert <luisa.kalkert@gmx.de>
2019-10-05 16:24:25 +0200

    HookTip alligned

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -54,7 +54,7 @@ public class GrapplingHook : MonoBehaviour
         {
             grapplingTarget = hit.point;
             hasTarget = true;
-            GameObject.Instantiate(hitMarker, grapplingTarget, transform.rotation);
+            GameObject.Instantiate(hitMarker, grapplingTarget, Camera.main.transform.rotation);
         }
     }
 
