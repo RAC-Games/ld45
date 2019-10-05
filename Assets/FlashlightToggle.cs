a49039ec5ad41a0d1ce8f43ca8edc38d5f5509ce
Justin <justin.osenberg@gmail.com>
2019-10-05 18:36:43 +0200

    Flashlight added

---
+++ Assets/FlashlightToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Flashlight"))
+        {
+            light.enabled = !light.enabled;
+        }
+    }
+}
