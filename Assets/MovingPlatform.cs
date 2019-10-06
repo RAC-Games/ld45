502ba39ef23b7d753c24f98a298cf9d0e7503bd2
Konrad <kgerisch@hs-koblenz.de>
2019-10-07 00:02:54 +0200

    movingPlatformPrototype

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    private bool goLeft;
+    private bool goRight;
+    public bool goRightAtStart;
+    public float cycleTime;
+    private float cycleStartTime;
+
+    private void Start()
+    {
+        cycleStartTime = Time.time;
+        if (goRightAtStart)
+        {
+            goRight = true;
+        } else
+        {
+            goLeft = true;
+        }
+    }
+
+    private void Update()
+    {
+        TurnAround();
+        if (goLeft)
+        {
+            transform.Translate(new Vector3(-Time.deltaTime, 0, 0));
+        } else
+        {
+            transform.Translate(new Vector3(Time.deltaTime, 0, 0));
+        }
+    }
+
+    void TurnAround()
+    {
+        if (Time.time > cycleStartTime + cycleTime)
+        {
+            cycleStartTime = Time.time;
+            goRight = !goRight;
+            goLeft = !goLeft;
+        }
+    }
+}
