bdb5fc7e1f42972440bf56d804b39785dd1ad165
Chris <c.rausch020@gmail.com>
2019-10-06 23:41:03 +0200

    lighting button

--- Assets/Scripts/Onpress_Button.cs
+++ Assets/Scripts/Onpress_Button.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Onpress_Button : MonoBehaviour{
- public UnityEvent OnButtonPressed=new UnityEvent();
- void OnTriggerEnter(Collider other){OnButtonPressed.Invoke();
- print(other);}
- }
+public class Onpress_Button : MonoBehaviour
+{
+    public UnityEvent OnButtonPressed = new UnityEvent();
+    void OnTriggerEnter(Collider other)
+    {
+        OnButtonPressed.Invoke();
+    }
+}
