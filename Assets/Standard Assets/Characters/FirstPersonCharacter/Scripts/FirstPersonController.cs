ac863ce8101f7ea482bb73613af98e4482a3e539
Chris <c.rausch020@gmail.com>
2019-10-06 23:00:29 +0200

    Update FirstPersonController.cs

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -102,7 +102,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 SetMouseSensitivity(options.MouseSensitivity);
             });
 
-                SetMouseSensitivity(options.MouseSensitivity);
+            SetMouseSensitivity(options.MouseSensitivity);
             SetFieldOfView(options.FieldOfView);
         }
 
