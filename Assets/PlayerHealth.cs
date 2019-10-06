ae46d2852b38d543a08c2ae687ce7f6309ad984b
Chris <c.rausch020@gmail.com>
2019-10-07 00:51:07 +0200

    damage , death animation added

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerHealth : MonoBehaviour
 {
     public HealthSO playerHealth;
+    public Image damage;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +16,25 @@ public class PlayerHealth : MonoBehaviour
             GetComponent<FirstPersonController>().Death();
         });
     }
-
+    Color oldColor;
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerHealth.GetDamage)
+        {
+        oldColor = damage.color;
+        oldColor.a = 1 - playerHealth.Health / playerHealth.MaxHealth;
+        damage.color = oldColor;
+        }
+        else
+        {
+            if (damage.color.a>Time.deltaTime)
+            {
+                oldColor = damage.color;
+                oldColor.a -= Time.deltaTime;
+                damage.color = oldColor;
+            }
+            
+        }
     }
 }
