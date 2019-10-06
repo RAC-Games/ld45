e72686382bf25644bb2e30d870d95bd8bec55bdc
L´s <to.buy@t-onlilne.de>
2019-10-06 20:41:40 +0200

    Level unpolished

---
+++ Assets/Scripts/Doortimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Doortimer : MonoBehaviour{
+public float timer=0;
+public int Maxzeit;
+public bool timer_is_running=false;
+private Animator animator;
+public Text text;
+
+void Start(){
+animator=GetComponent<Animator>();}
+   
+    void Update()
+    {
+	if (timer_is_running==true){
+		 timer -= Time.deltaTime;
+		 text.text = timer.ToString("##");
+		 if (0>timer)
+		 {
+		 close_door();
+		 timer_is_running=false;}
+	}}
+
+	public void starttimer(){
+	timer=Maxzeit;
+	timer_is_running=true;
+
+	}
+	public void close_door(){
+	animator.SetTrigger("Door_close");
+	}
+	
+	public void open_door(){
+		animator.SetTrigger("Door_open");
+	}
+
+}
