55e21372f100604e4159818d7f5110e9d0b9f208
Chris <c.rausch020@gmail.com>
2019-10-06 21:37:25 +0200

    3d text shader added

--- Assets/Scripts/Doortimer.cs
+++ Assets/Scripts/Doortimer.cs
@@ -3,38 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Doortimer : MonoBehaviour{
-public float timer=0;
-public float Maxzeit;
-public bool timer_is_running=false;
-private Animator animator;
-public Text text;
+public class Doortimer : MonoBehaviour
+{
+    public float timer = 0;
+    public float Maxzeit;
+    public bool timer_is_running = false;
+    private Animator animator;
+    public TextMesh text;
 
-void Start(){
-animator=GetComponent<Animator>();}
-   
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-	if (timer_is_running==true){
-		 timer -= Time.deltaTime;
-		 text.text = timer.ToString("##");
-		 if (0>timer)
-		 {
-		 close_door();
-		 timer_is_running=false;}
-	}}
+        if (timer_is_running == true)
+        {
+            timer -= Time.deltaTime;
+            text.text = timer.ToString("##");
+            if (0 > timer)
+            {
+                close_door();
+                timer_is_running = false;
+                text.text = "";
+            }
+        }
+    }
 
-	public void starttimer(){
-	timer=Maxzeit;
-	timer_is_running=true;
+    public void starttimer()
+    {
+        timer = Maxzeit;
+        timer_is_running = true;
 
-	}
-	public void close_door(){
-	animator.SetTrigger("Door_close");
-	}
-	
-	public void open_door(){
-		animator.SetTrigger("Door_open");
-	}
+    }
+    public void close_door()
+    {
+        animator.SetTrigger("Door_close");
+    }
+
+    public void open_door()
+    {
+        animator.SetTrigger("Door_open");
+    }
 
 }
