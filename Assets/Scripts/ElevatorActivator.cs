47dc4f58d62c0ba84215076f58eeb6c5ec110e2a
Unknown <lia.liberada@gmail.com>
2019-10-06 15:04:53 +0200

    Rotor
    Blend Modell with 2 Material slots

---
+++ Assets/Scripts/ElevatorActivator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorActivator : MonoBehaviour
+{
+    public GameObject Player;
+    // public GameObject Activator;
+    private Rigidbody rigidbody_player;
+
+    //void Awake()
+    //{
+    //    rigidbody_player = Player.gameObject.GetComponent<Rigidbody>();
+
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        // Change the cube color to green.
+        MeshRenderer meshRend = GetComponent<MeshRenderer>();
+        meshRend.material.color = Color.green;
+        Debug.Log("Trigger activated");
+    }
+
+}
