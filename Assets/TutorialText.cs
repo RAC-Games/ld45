197489f9fdd33c16954259514d779082f1009832
Luisa Kalkert <luisa.kalkert@gmx.de>
2019-10-07 12:41:33 +0200

    Animated Tutuorial Text

--- Assets/TutorialText.cs
+++ Assets/TutorialText.cs
@@ -7,20 +7,27 @@ using UnityStandardAssets.CrossPlatformInput;
 public class TutorialText : MonoBehaviour
 {
     public GameObject text;
+    public GameObject[] otherTexts;
 
     private void OnTriggerEnter(Collider other)
     {
         
         if (other.CompareTag("Player"))
         {
+            foreach(GameObject t in otherTexts)
+            {
+                t.SetActive(false);
+            }
             text.SetActive(true);
+            this.GetComponent<Collider>().enabled = false;
         }
     }
 
     private void Update()
     {
-        if ((CrossPlatformInputManager.GetButton("Submit") && text.activeSelf) || (Input.GetKey(KeyCode.E) && text.activeSelf))
+        if (CrossPlatformInputManager.GetButton("Submit") && text.activeSelf)
         {
+            
             text.SetActive(false);
             this.gameObject.SetActive(false);
         }
